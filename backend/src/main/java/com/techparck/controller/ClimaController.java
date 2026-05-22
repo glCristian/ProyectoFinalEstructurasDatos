acdce0c49545710ae9e27c68321f8135483becce
jospina06 <juanj.ospinav@uqvirtual.edu.co>
2026-05-22 15:10:37 -0500

    Dependencias necesarias

---
+++ backend/src/main/java/com/techparck/controller/ClimaController.java
@@ -0,0 +1,31 @@
+package main.java.com.techparck.controller;
+
+//import com.google.gson.Gson;
+//import main.java.com.techpark.service.ParqueService;
+//import io.javalin.Javalin;
+
+//import java.util.List;
+//import java.util.Map;
+
+public class ClimaController {
+
+    //private final ParqueService parqueService = ParqueService.getInstance();
+    //private final Gson gson = new Gson();
+//
+    //public void registrarRutas(Javalin app) {
+//
+    //    // GET /clima → estado climático actual
+    //    app.get("/clima", ctx ->
+    //        ctx.json(gson.toJson(Map.of(
+    //            "alertaActiva", parqueService.isAlertaClimatica(),
+    //            "tipoAlerta",   parqueService.getTipoAlerta()
+    //        ))));
+    //    // DELETE /clima/alerta → desactivar alerta climática
+    //    app.delete("/clima/alerta", ctx -> {
+    //        parqueService.desactivarAlertaClimatica();
+    //        ctx.result("Alerta climática desactivada");
+    //    });
+    //}
+}
+
+
---
+++ backend/src/main/java/com/techparck/service/RutaService.java
@@ -0,0 +1,5 @@
+package main.java.com.techparck.service;
+
+public class RutaService {
+    
+}
